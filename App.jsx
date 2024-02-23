23abc99d35c06d0a36e848047619eaaeea40c2e1
Aditya Swami <142524039+aadltya@users.noreply.github.com>
2024-02-23 16:48:58 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,43 @@
+import { useContext, useState } from 'react'
+import { CountContext } from './context';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <CountContext.Provider value={count} >
+        <Count count={count} setCount={setCount}/>
+      </CountContext.Provider>
+    </div>
+  )
+}
+
+function Count({setCount}) {
+  return <div>
+    <CountRenderer />
+    <Buttons setCount={setCount} />
+  </div>
+}
+
+function CountRenderer() {
+  const count = useContext(CountContext);
+  return <div>
+    {count}
+  </div>
+}
+
+function Buttons({setCount}) {
+  const count = useContext(CountContext);
+  return <div>
+    <button onClick={() => {
+      setCount(count-1)
+    }} >Decrease Count</button>
+    <button onClick={() => {
+      setCount(count+1)
+    }} >Increase Count</button>
+  </div>
+}
+
+export default App
